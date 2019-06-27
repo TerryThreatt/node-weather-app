42fc3f3af337b4594f538907d83be29901e17d4d
Terry Threatt <terry.threatt@gmail.com>
2019-06-27 02:28:21 -0500

    updated about forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ request({ url, json: true }, (error, { body }) => {
     } else if (body.error) { 
         callback('Unable to find location.', undefined);
     } else {
-        callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees. '+ ' There is a ' +   body.currently.precipProbability * 100 + '% chance of rain.'
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees. The high today is ' + body.daily.data[0].temperatureHigh +' degrees' + ' with a low of ' + body.daily.data[0].temperatureLow + ' degrees.' + ' There is a ' +   body.currently.precipProbability * 100 + '% chance of rain.'
         );
      }
  });
